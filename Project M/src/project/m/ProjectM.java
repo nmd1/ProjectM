2b9ad8961544765b6d5d05f7ab9b5df92f36d6e2
nmd1 <rafty132@gmail.com>
2015-10-31 12:54:06 -0400

    First Commit
    A Program designed to take strokes from a keyboard and turns it into
    letters and words

---
+++ Project M/src/project/m/ProjectM.java
@@ -0,0 +1,17 @@
+
+package project.m;
+
+/**
+ *
+ * @author User
+ */
+public class ProjectM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
