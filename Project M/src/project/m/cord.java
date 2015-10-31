0daff1061117afdccb017a50ee09312b60f95b91
nmd1 <rafty132@gmail.com>
2015-10-31 16:02:25 -0400

    Created cord class
    Created the cord class with time and position coordinates

---
+++ Project M/src/project/m/cord.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.m;
+
+/**
+ *
+ * @author User
+ */
+public class cord {
+    double time;
+    int x;
+    int y;
+    public cord(double time, int x, int y) {
+        
+    }
+    
+    public void setTime(int t){
+        time = t;
+    }
+    
+    public void setX(int x1) {
+        x = x1;
+    }
+    
+    public void setY(int y1) {
+        y = y1;
+    }
+    
+    
+}
