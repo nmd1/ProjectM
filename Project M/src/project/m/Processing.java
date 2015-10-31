aea2711bcce72f79e3731c504c856eed5111ea0f
max7098 <max.mesirow@gmail.com>
2015-10-31 16:26:45 -0400

    keystroke enum
    added keystroke enum, to break down letters

--- Project M/src/project/m/Processing.java
+++ Project M/src/project/m/Processing.java
@@ -1,9 +1,41 @@
 package project.m;
 
+import java.util.ArrayList;
+
 public class Processing{
 String text;
+final double timeVarience = 1.2;//TODO
 public Processing()
 {
 	text = "";
+}
+public char inputText(ArrayList<cord> grid)
+{
+	shift(grid);//makes it all left bound
+	ArrayList<keystroke> swipes = new ArrayList<keystroke>();
+	
+	
+	return 'a';
+}
+
+
+public static void shift(ArrayList<cord> cordinates)//shifts to leftmost position
+{
+	int smallestX = cordinates.get(0).getX();
+	for(cord s:cordinates)//calculates the smallest x value in the array
+	{
+		if(s.getX()<smallestX)
+		{
+			smallestX = s.getX();
+		}
+	}
+	
+	if(smallestX>0)
+	{
+		for(cord s:cordinates)//Moves all elements to the left until one of them is left most
+		{
+			s.setX(s.getX()-smallestX);
+		}
+	}
 }
 }
---
+++ Project M/src/project/m/keystroke.java
@@ -0,0 +1,5 @@
+package project.m;
+
+public enum keystroke {
+	Backslash,Forwardslash,UnkownSlash,CCurve,DCurve,DashH,DashM,DashL,Btop,Bbot,JCurve,KTop,Kbot,SCurve,UCurve,YSlash
+}
