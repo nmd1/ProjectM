125332a44a9aefb3739bc87303119c74018ad559
nmd1 <rafty132@gmail.com>
2015-10-31 22:13:25 -0400

    Updated cord file
    Copied the temp cord class with the actual one

--- Project M/src/project/m/cord.java
+++ Project M/src/project/m/cord.java
@@ -1,22 +1,39 @@
+
 package project.m;
 
+
 public class cord {
-	int getX()
-	{
-		return 0;
-	}
-	int getY()
-	{
-		return 0;
-	}
-	double getT()
-	{
-		return 0.0;
-	}
-	
-	void setX(int x)
-	{
-		
-	}
-	
+    double time;
+    int x;
+    int y;
+    public cord(long t, int x1, int y1) {
+        time = t;
+        x = x1;
+        y = y1;
+    }
+    
+    public void setTime(long t){
+        time = t;
+    }
+    
+    public void setX(int x1) {
+        x = x1;
+    }
+    
+    public void setY(int y1) {
+        y = y1;
+    }
+    
+    public double getTime () {
+        return time;
+    }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }
 }
+
